570ac57c9949e8cbe44cb9112a9784830a0e9b8c
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-01 20:13:16 +0530

    task:servicepayment structre change

--- src/models/servicePayments.js
+++ src/models/servicePayments.js
@@ -2,25 +2,28 @@ const { default: mongoose } = require("mongoose");
 const itemDataSchema = require("./itemDataSchema");
 const servicePaymentsSchema = mongoose.Schema(
   {
-    amountPaid: {
+    totalBillGenerated: {
       type: Number,
       required: true,
       default: 0,
     },
-    dueAmount: {
-      type: Number,
-      default: 0,
-    },
-    cashPay: {
-      type: Number,
-      default: 0,
-    },
-    onlinePay: {
-      type: Number,
-      default: 0,
-    },
     list: {
-      type: [itemDataSchema],
+      type: [
+        {
+          amountPaid: {
+            type: Number,
+            default: 0,
+          },
+          cashPay: {
+            type: Number,
+            default: 0,
+          },
+          onlinePay: {
+            type: Number,
+            default: 0,
+          },
+        },
+      ],
     },
   },
   {
